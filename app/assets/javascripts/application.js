f620f40106e6ef5600d0586be2ce140b0249296f
Mike Talley <miketalley@hotmail.com>
2014-05-10 17:01:28 -0400

    WINNAZZ!!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,34 @@
+
+
+$(document).ready(function () {
+$("a.1").click(function(){
+  $("a.1").addClass("active");
+  $("a.2").removeClass("active");
+  $("a.5").removeClass("active");
+});
+
+$("a.2").click(function(){
+  $("a.2").addClass("active");
+  $("a.1").removeClass("active");
+  $("a.5").removeClass("active");
+});
+
+$("a.5").click(function(){
+  $("a.5").addClass("active");
+  $("a.1").removeClass("active");
+  $("a.2").removeClass("active");
+});
+});
+
+
+
+
+
+
+
+
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
