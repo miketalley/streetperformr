825e2fe75b05d571efd750e94902a10595d5344a
Lisa Plesko <plesko.l@gmail.com>
2014-05-10 09:48:19 -0400

    Add new column to Artist for photo url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510004513) do
+ActiveRecord::Schema.define(version: 20140510134335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140510004513) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "location_id"
+    t.text     "photo_url"
   end
 
   create_table "locations", force: true do |t|
