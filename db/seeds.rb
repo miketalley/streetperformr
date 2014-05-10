1605d4808f36f4e93b190f5bfc5e4a58a9ca9131
Mike Talley <miketalley@hotmail.com>
2014-05-10 15:01:03 -0400

    clearned up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
- # t.text     "artist"
- #    t.integer  "rating"
- #    t.integer  "tip"
- #    t.text     "review"
- #    t.text     "genre"
- #    t.datetime "created_at"
- #    t.datetime "updated_at"
- #    t.integer  "location_id"
 Artist.delete_all
+User.delete_all
 
 artist = Artist.create(artist: 'Alakazam',
                         rating: 4,
@@ -94,29 +87,4 @@ user = User.create(latitude: 42.3561109,
                 title: 'Ten31 Productions',
                 description: 'Scared the hell outta me! They are real!',
                 address: 'Boston Commons, Boston')
-# artist = Artist.create(artist: 'Tim', rating: 3, tip: 0, review: 'Love his style.', genre: 'music')
-# artist = Artist.create(artist: 'Freddy and the Steppers', rating: 4, tip: 2, review: 'Love his music.', genre: 'music')
-# artist = Artist.create(artist: 'Johnny', rating: 3, tip: 2, review: 'Great Saturday morning entertainment.', genre: 'music')
-# artist = Artist.create(artist: 'Corner Street Performers', rating: 3, tip: 0, review: 'Love the location.', genre: 'carnival')
-# artist = Artist.create(artist: 'Fifth Ave Quartest', rating: 5, tip: 4, review: 'Love his music.', genre: 'performer')
-# artist = Artist.create(artist: 'Boulevard Boys', rating: 2, tip: 2, review: 'Love his moves!', genre: 'carnival')
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Location.create(name: "New york" ,state: "NY", population: 8175133)
-# Location.create(name: "Los Angeles",state: "CA", population: 3792621 )
-# Location.create(name: "Chicago",state: "IL", population: 2695598)
-# Location.create(name: "Houston",state: "TX", population: 2099451)
-# Location.create(name: "Philadelphia",state: "PA" , population: 1526006)
-# Location.create(name: "Phoenix",state: "AZ", population: 1445632)
-# Location.create(name: "San Antonio",state: "TX", population: 1327407)
-# Location.create(name: "San Diego",state: "CA", population: 1307402 )
-# Location.create(name: "Dallas",state: "TX", population: 1197816)
-# Location.create(name: "San Jose",state: "CA", population: 945942)
 
