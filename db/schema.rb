fc9f9297d0965d718a9f58ec04b54ce79131c7e3
Mike Talley <miketalley@hotmail.com>
2014-05-15 15:35:29 -0400

    Modified seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511184627) do
+ActiveRecord::Schema.define(version: 20140510004513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,22 +22,14 @@ ActiveRecord::Schema.define(version: 20140511184627) do
     t.integer  "tip"
     t.text     "review"
     t.text     "genre"
+    t.text     "photo_url"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "location_id"
-    t.text     "photo_url"
-    t.text     "description"
   end
 
   create_table "locations", force: true do |t|
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "address"
-    t.text     "description"
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "gmaps",       default: true
   end
 
 end
